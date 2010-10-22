5cb33cd0c1feab2af7c86f72a1a2129a8aa115ad
Martin Doktár <martin@xtreme.se>
2010-10-22 12:24:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CasProxy::Application.config.secret_token = '7da1c16adc6478aeb05b0780de218315430c72f2b5546578db6d6e21cf49a1d4dbedf144b94d60416774c3132c75c3035c616b30227a27d8bd065dd32520ea2b'
