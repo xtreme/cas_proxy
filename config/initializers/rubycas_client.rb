bbb5b95367bf2cb5cfb6bb6bb9ffaaad7dead720
Martin Doktár <martin@xtreme.se>
2010-10-22 23:37:38 +0200

    Remove active record

--- config/initializers/rubycas_client.rb
+++
@@ -1,7 +0,0 @@
-require 'casclient'
-require 'casclient/frameworks/rails/filter'
-
-CASClient::Frameworks::Rails::Filter.configure(
-  :server_name => "login_proxy.xtreme.se",
-  :server_port => 444
-)
